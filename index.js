61bba62c0a238b5eeb6895180f387cb684a9f4db
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-08 22:39:47 +0600

    service route not found error handling

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middlewares
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("service-review-server is running");
+});
+
+app.get("*", function (req, res) {
+  res.status(404).send("service route not found");
 });
 
 app.listen(port, () => {
