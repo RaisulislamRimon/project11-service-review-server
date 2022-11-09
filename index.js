22e2d493974555e4d5ba3e04a743fc8a3048cd70
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-09 17:25:03 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const services = require("./services.json");
+const services = require("https://service-review-serverce-assignment");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.txudmws.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
   res.send("service-review-server is running");
